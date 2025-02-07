78c9348fcaf941caafb7d65efb2160b9635ba0d2
IanSolorio <isolorio22@unamad.edu.pe>
2025-02-07 14:58:54 -0500

    Otra configuracion

--- Parcial/vite.config.js
+++ Parcial/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://IanSolorio.github.io/WebParcial',
+  base: "/WebParcial",
   plugins: [react()],
 })
