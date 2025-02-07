701d99d9c7ae8433ac4b169b29be7807c7e54b26
IanSolorio <isolorio22@unamad.edu.pe>
2025-02-07 15:05:52 -0500

    Ultimo Commit

--- Parcial/src/main.jsx
+++ Parcial/src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'; // Si tienes algún estilo adicional
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
